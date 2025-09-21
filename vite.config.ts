21d658a22d9c8d8fd130e136e97b9917d21c91a0
amarzeus <amarmahakal92@gmail.com>
2025-09-22 00:35:06 +0530

    required fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     define: {
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
   }
 });
